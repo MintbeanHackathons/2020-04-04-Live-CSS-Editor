6b17e14f9030404a0a45a72dc51245cc026175ce
Bryce McMath <bryce.j.mcmath@gmail.com>
2020-04-04 10:25:04 -0700

    Get editor working, add dropdown for lang select

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -1,13 +1,43 @@
 <template>
   <div>
-    <p>Hello world!</p>
+    <p>Mintbean Editor 2020-04-04 Bryce McMath</p>
+    <select name="langs" id="langs">
+      <option v-for="(opt, i) in langOpts" :key="i" :value="opt">{{opt}}</option>
+    </select>
+
+    <editor
+      v-model="content"
+      @init="editorInit"
+      :lang="lang"
+      theme="chrome"
+      width="500"
+      height="100"
+    ></editor>
   </div>
 </template>
 
 <script>
 export default {
+  name: 'Popup',
+  components: {
+    editor: require('vue2-ace-editor'),
+  },
   data() {
-    return {};
+    return {
+      content: '',
+      langOpts: ['css', 'html'],
+      lang: 'css',
+    };
+  },
+  methods: {
+    editorInit: function() {
+      require('brace/ext/language_tools'); //language extension prerequsite...
+      require('brace/mode/html');
+      require('brace/mode/javascript'); //language
+      require('brace/mode/less');
+      require('brace/theme/chrome');
+      require('brace/snippets/javascript'); //snippet
+    },
   },
 };
 </script>
