68c18ce87f34319b7138e7b45a14eb75915e15a0
unknown <mrloganellis@gmail.com>
2020-04-04 14:38:39 -0400

    almost finished hackathon project, need to figure out how to connect stylesheet to html sheet

---
+++ index.js
@@ -0,0 +1,67 @@
+let cssEditor = ace.edit("cssEditor");
+cssEditor.getSession().setMode("ace/mode/css");
+let htmlEditor = ace.edit("htmlEditor");
+htmlEditor.getSession().setMode("ace/mode/html")
+
+function getTextValue() {
+  $('#editorValue').append(() => {
+    let code = htmlEditor.getValue();
+    return code;
+  })
+  $('#cssEditorValue').append(() => {
+    let cssCode = cssEditor.getValue()
+    return cssCode;
+  })
+  $('#htmlEditor').on('change', function() {
+    $('#editorValue').empty();
+    $('#editorValue').append(() => {
+      let code = htmlEditor.getValue();
+      let cssCode = cssEditor.getValue()
+      return code;
+    })
+  })
+  $('#cssEditor').on('change', function() {
+    $('#cssEditorValue').empty();
+    $('#cssEditorValue').append(() => {
+      let cssCode = cssEditor.getValue()
+      return cssCode;
+    })
+  })
+  
+}
+
+
+$(getTextValue)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function themeChooser() {
+//   let theme = editor.setTheme("ace/theme/github");
+//   $('.lightMode').on('click', () => {
+//     theme = editor.setTheme("ace/theme/github");
+//   })
+//   $('darkMode').on('click', () => {
+//     theme = editor.setTheme("ace/theme/twilight");
+//   })
+//   return theme;
+// }
+
+
+
+// $(themeChooser)
