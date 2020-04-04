9a111f9d5f0914c73066ce09d8c045be2e1d3760
Robert Nafrada <robnafrada@gmail.com>
2020-04-04 15:57:29 -0400

    minor edits

--- live-css-editor/src/App.js
+++ live-css-editor/src/App.js
@@ -1,6 +1,5 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import { useState } from 'react';
 
 
 
@@ -8,17 +7,26 @@ function App() {
   // const style = '* {color: red; font-size: 10px;}';
   const[style, setStyle] = useState("body { background-color: #FFF;}")
 
+  
+
+  const editTextarea = "textarea { width: 100px; height: 100px;}"
+
   const handleOnChange = (event) => {
     setStyle(event.target.value)
   }
   
   return (
     <div className="App">
-      <h1>Live Edit CSS</h1>
+      <h1>LIVE CSS EDITOR</h1>
+      <p>Do you want to learn how to style a page with CSS? You can do so below!</p>
+      <style contentEditable={true} dangerouslySetInnerHTML={{__html: style}}></style>
+      <textarea dangerouslySetInnerHTML={{__html: editTextarea}} onChange={handleOnChange} placeholder="body { background: red; }"/>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <div>Hi, if you would like to change the styling on this particular page. You can do so below:</div>
-      <style dangerouslySetInnerHTML={{__html: style}}></style>
-      <textarea contentEditable={true} onChange={handleOnChange} placeholder="body { background: red; }"/>
+      <ul>
+        <li>Example List Item 1</li>
+        <li>Example List Item 2</li>
+        <li>Example List Item 3</li>
+      </ul>
     </div>
   );
 }
