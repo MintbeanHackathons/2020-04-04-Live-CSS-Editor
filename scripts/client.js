5eb755e58073865b0c5fb0b8f25be0193e5d41e0
haley-r <haleyryan77@gmail.com>
2020-04-04 13:50:14 -0500

    using ace editor, can change styles of webpage as user types

--- scripts/client.js
+++ scripts/client.js
@@ -1,1 +1,22 @@
-console.log('hi from client');
+$(readyNow);
+
+// STRATEGY
+// editor.getValue(); gets the value of what's in the editor
+// $('#dynamic-styles').text(__CUSTOM_TEXT____) sets the value of the input in the editor
+
+
+function readyNow(){
+    //set up event listener on editor
+    $('#editor').on('input', getValue);  
+}
+
+//get the value of the editor, then update the style tag with updateStyle
+function getValue(){
+    valueOfEditor=editor.getValue();
+    updateStyle(valueOfEditor);    
+}
+
+//insert a value into style tag in html
+function updateStyle(customText){
+    $('#dynamic-styles').text(customText)
+}
