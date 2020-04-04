4269765609f1a5aae4b5f59e8fcba2263f1dc362
Anthony J. DeMeglio <22527727+ademeglio@users.noreply.github.com>
2020-04-04 16:12:20 -0400

    Starting js code

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+
 // Code to read editor
+var editor = document.getElementById("editor")
+var editorContent = editor.innerHTML
 
-
+// Edited Content
+let updatedContent = onChange(editorContent, () => save(editorContent));
 
 // Code to write editor output to style
+var pageStyle = document.getElementById("page-style")
+pageStyle.innerHTML = editorContent;
+
 
