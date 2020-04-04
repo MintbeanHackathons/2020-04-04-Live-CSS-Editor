bd7b84e2debf3bbba49ff1633623a99cf2f39f39
Leah H <harrichl@gmail.com>
2020-04-04 14:39:09 -0400

    simple css editor using ace

---
+++ cssEditor.js
@@ -0,0 +1,47 @@
+
+function setupEditor() {
+    window.editor = ace.edit("editor");
+    editor.setTheme("ace/theme/monokai");
+    editor.getSession().setMode("ace/mode/html");
+    editor.setValue(`<!DOCTYPE html>
+    <html>
+    <head>
+    </head>
+    <style>
+
+    </style>
+
+    <body>
+    </body>
+
+    </html>`,1); //1 = moves cursor to end
+
+    editor.getSession().on('change', function() {
+        update();
+    });
+
+    editor.focus();
+    
+    
+    editor.setOptions({
+        fontSize: "11pt",
+        showLineNumbers: false,
+        showGutter: false,
+        vScrollBarAlwaysVisible:true,
+        enableBasicAutocompletion: false, enableLiveAutocompletion: false
+    });
+
+    editor.setShowPrintMargin(false);
+    editor.setBehavioursEnabled(false);
+}
+
+function update() {
+    let idoc = document.getElementById("iframe").contentWindow.document;
+
+    idoc.open();
+    idoc.write(editor.getValue());
+    idoc.close();
+}
+
+setupEditor();
+update();
