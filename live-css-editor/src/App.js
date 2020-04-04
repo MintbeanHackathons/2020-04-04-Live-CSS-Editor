64b6f35853f8ff75e0678775f0e626b9d1f45e1b
Robert Nafrada <robnafrada@gmail.com>
2020-04-04 16:05:46 -0400

    minor fixes

--- live-css-editor/src/App.js
+++ live-css-editor/src/App.js
@@ -7,9 +7,6 @@ function App() {
   // const style = '* {color: red; font-size: 10px;}';
   const[style, setStyle] = useState("body { background-color: #FFF;}")
 
-  
-
-  const editTextarea = "textarea { width: 100px; height: 100px;}"
 
   const handleOnChange = (event) => {
     setStyle(event.target.value)
@@ -20,7 +17,7 @@ function App() {
       <h1>LIVE CSS EDITOR</h1>
       <p>Do you want to learn how to style a page with CSS? You can do so below!</p>
       <style contentEditable={true} dangerouslySetInnerHTML={{__html: style}}></style>
-      <textarea dangerouslySetInnerHTML={{__html: editTextarea}} onChange={handleOnChange} placeholder="body { background: red; }"/>
+      <textarea onChange={handleOnChange} placeholder="body { background: red; }"/>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       <ul>
         <li>Example List Item 1</li>
