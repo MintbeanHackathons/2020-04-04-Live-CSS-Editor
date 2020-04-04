1e6e65f8d0a55f0c8d8db74f1835eabd08a32412
Max <montrealist@users.noreply.github.com>
2020-04-04 12:57:05 -0400

    added ace editor

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+// import logo from "./logo.svg";
+// import "./App.css";
+
+import AceEditor from "react-ace";
+
+import "ace-builds/src-noconflict/mode-css";
+import "ace-builds/src-noconflict/theme-github";
+
+const code = `.sample-markup {
+  font-size: 2em;
+  color: purple;
+}`;
+
+const sheet = document.createElement("style");
+sheet.innerHTML = code;
+document.body.appendChild(sheet);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [style, setStyle] = useState(code);
+    function onChange(newValue) {
+        setStyle(newValue);
+        sheet.innerHTML = newValue;
+        // console.log("sheet", sheet);
+    }
+    return (
+        <div className="App">
+            <div className="editor">
+                <AceEditor
+                    mode="css"
+                    value={style}
+                    theme="github"
+                    onChange={onChange}
+                    name="UNIQUE_ID_OF_DIV"
+                    setOptions={{ showLineNumbers: true }}
+                    editorProps={{ $blockScrolling: true }}
+                />
+            </div>
+            <div className="sample-markup">
+                <h1>I am heading</h1>
+                <p>I am paragraph.</p>
+            </div>
+        </div>
+    );
 }
 
 export default App;
