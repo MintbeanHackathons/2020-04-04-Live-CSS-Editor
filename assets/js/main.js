62344dfa533534e167b2f482f5d20636f8f04063
dipto0321 <diptokmk47@gmail.com>
2020-04-05 00:35:51 +0600

    :sparkles: Add css changer for global

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,17 @@
-console.log("Hello");
+const cssEditor = document.getElementById("css-editor");
+const head = document.head;
+const style = document.createElement("style");
+
+style.type = "text/css";
+head.insertAdjacentElement("beforeend", style);
+
+const editorKeyupEvent = () => {
+  editorValue = cssEditor.value;
+  try {
+    style.innerText = editorValue;
+  } catch (error) {
+    console.log("Error", error);
+  }
+};
+
+cssEditor.addEventListener("keyup", editorKeyupEvent);
