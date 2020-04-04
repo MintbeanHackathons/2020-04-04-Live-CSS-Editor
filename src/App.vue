62ca310ebf825ea26f9727ce80b90710296668af
Derek Johnston <Derek@dj-design.ca>
2020-04-04 13:20:55 -0400

    setup a couple tags and set default component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+  #app
+    CustomCSS
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CustomCSS from './components/CustomCSS.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CustomCSS
   }
 }
 </script>
