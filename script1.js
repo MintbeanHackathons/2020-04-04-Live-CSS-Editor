185bf5389619c41c79b10f3bdf75079202da8fb8
Daryl Marco <darmarco@gmail.com>
2020-04-04 20:02:17 -0700

    Refactored so that parsing CSS wasn't necessary

--- script1.js
+++ script1.js
@@ -1,28 +1,8 @@
 var cssInput = document.querySelector('#cssInput');
 cssInput.addEventListener('input', (e) => {
-  styleParser(e.target.value);
+  updateStyle(e.target.value);
 })
-function styleParser(text) {
-  //break up into sections based on ending curlybrackets
-  var allElements = Array.from(document.querySelectorAll('*g'));
-  allElements.forEach (a => {a.removeAttribute('style')})
-  var splitText = text.split('}')
-  for (let i = 0; i < splitText.length; i++) {
-    var section = splitText[i].split('{');
-    console.log('section',section)
-    if (!section[1]){
-      return;
-    }
-    var selection = document.querySelectorAll(section[0])
-    var selectionArray = Array.from(selection);
-    selectionArray.forEach( a => {
-      console.log('selection',a)
-      var properties = section[1].split(';')
-      for (let j = 0; j < properties.length; j++) {
-        let splitProps = properties[j].split(':')
-        a.style.setProperty(splitProps[0].trim(), splitProps[1])
-      }
-    })
-  }
-  //for each item in splitText array,
+function updateStyle(text) {
+  var styleText = document.querySelector("style");
+  styleText.innerHTML = text;
 }
