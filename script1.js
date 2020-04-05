0d139ee659252d5f4ffd5b2fd0f684061522728e
Daryl Marco <darmarco@gmail.com>
2020-04-04 21:06:02 -0700

    Changed editor from textarea to ace editor

--- script1.js
+++ script1.js
@@ -1,8 +1,9 @@
-var cssInput = document.querySelector('#cssInput');
-cssInput.addEventListener('input', (e) => {
-  updateStyle(e.target.value);
-})
-function updateStyle(text) {
-  var styleText = document.querySelector("style");
-  styleText.innerHTML = text;
-}
+// var cssInput = document.querySelector('#editor');
+// console.log(cssInput);
+// cssInput.addEventListener('input', (e) => {
+//   updateStyle(e.target.getvalue);
+// })
+// function updateStyle(text) {
+//   var styleText = document.querySelector("style");
+//   styleText.innerHTML = text;
+// }
