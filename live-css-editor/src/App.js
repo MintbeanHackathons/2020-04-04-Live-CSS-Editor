2d573e06f3c12f72574cc513351ebd190304e737
Robert Nafrada <robnafrada@gmail.com>
2020-04-16 16:18:42 -0400

    Small revisions to the styling

--- live-css-editor/src/App.js
+++ live-css-editor/src/App.js
@@ -1,10 +1,10 @@
 import React, {useState} from 'react';
-import './App.css';
+import './App.scss';
 
 
 
 function App() {
-  // const style = '* {color: red; font-size: 10px;}';
+  
   const[style, setStyle] = useState("body { background-color: #FFF;}")
 
 
@@ -13,17 +13,29 @@ function App() {
   }
   
   return (
-    <div className="App">
-      <h1>LIVE CSS EDITOR</h1>
-      <p>Do you want to learn how to style a page with CSS? You can do so below!</p>
-      <style contentEditable={true} dangerouslySetInnerHTML={{__html: style}}></style>
-      <textarea onChange={handleOnChange} placeholder="body { background: red; }"/>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <ul>
-        <li>Example List Item 1</li>
-        <li>Example List Item 2</li>
-        <li>Example List Item 3</li>
-      </ul>
+    <div className="editor-page">
+      <h1 id="title">LIVE CSS EDITOR</h1>
+
+      <div className="editor-page__container">
+        <div className="editor-page__edit-container">
+          <p id="main-par">Do you want to learn how to style a page with CSS? Try changing the styling for these HTML tags, h1, p, ul and il!</p>
+          <style  dangerouslySetInnerHTML={{__html: style}}></style>
+          <textarea className="editor-page__edit-container--textarea" onChange={handleOnChange} placeholder="h1 { color: red; }"/>
+        </div>
+        
+        <div className="editor-page__example-container">
+          <h1>Example Title</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <ul>Example Unordered List
+            <li>Example List Item 1</li>
+            <li>Example List Item 2</li>
+            <li>Example List Item 3</li>
+          </ul>
+        </div>
+      </div>
+
+     
+     
     </div>
   );
 }
